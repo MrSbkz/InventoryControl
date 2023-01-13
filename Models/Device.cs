9faf540b4fa5a6d54a5a99e7cbe673ccea0cce1f
“Alex <korolenkosasha644@gmail.com>
2023-01-13 13:36:02 +0300

    Add Device and Employee Models

---
+++ Models/Device.cs
@@ -0,0 +1,14 @@
+﻿namespace InventoryControl.Models;
+
+public class Device
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; } = string.Empty;
+    
+    public Employee AssignedTo { get; set; }
+    
+    public DateTime RegisterDate { get; set; }
+    
+    public DateTime DecommissionDate { get; set; }
+}
