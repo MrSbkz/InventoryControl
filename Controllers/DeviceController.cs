ba9bc286293e6e5bd05f3bf1583a86610d597448
“Alex <korolenkosasha644@gmail.com>
2023-01-13 13:50:04 +0300

    Add DeviceController

---
+++ Controllers/DeviceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryControl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeviceController : ControllerBase
+    {
+        [HttpGet]
+        [Route("list")]
+        public async Task GetDeviceAsync()
+        {
+            
+        }
+    }
+}
+
